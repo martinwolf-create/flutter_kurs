750577f074ebca630c6de83f17947690d5ba3007
martinwolf-create <wolfmartin707@gmail.com>
2025-06-12 15:11:23 +0200

    korektur 3.4.3

--- 3.4.3/main.dart
+++ 3.4.3/main.dart
@@ -6,34 +6,70 @@ void main() {
     {'temp': null, 'rain': 3.8, 'wind': null},
   ];
   // Erstelle leere Liste für Temperaturen
-  List<double?> temps = [];
-  temps.add(weatherData[0]['temp'] ?? 0);
-  temps.add(weatherData[1]['temp'] ?? 0);
-  temps.add(weatherData[2]['temp'] ?? 0);
 
-  // Füge die Temperaturen der Wetterdaten in die Liste ein temps.add(weatherData[0][ 'temp']); temps.add(weatherData[1]['temp']); temps.add(weatherData[2]['temp']);
-  // Berechne den Durchschnitt der Temperaturen
-  double? avgTemp = (temps[0]! + temps[1]! + temps[2]!) / 3;
-  print('Durchschnittstemperatur: $avgTemp');
-  // Erstelle leere Liste für Niederschläge
-  List<double?> rains = [];
-  rains.add(weatherData[0]['rain'] ?? 0);
-  rains.add(weatherData[1]['rain'] ?? 0);
-  rains.add(weatherData[2]['rain'] ?? 0);
-  // Berechne den Durchschnitt der Niederschläge
-  double? avgRain = (rains[0]! + rains[1]! + rains[2]!) / 3;
-  print('Durchschnittlicher Niederschlag: $avgRain');
+  double tempSum = 0;
+  int anzahl = 0;
+
+  if (weatherData[0]["temp"] != null) {
+    tempSum += weatherData[0]["temp"]!;
+    anzahl++;
+  }
+  if (weatherData[1]["temp"] != null) {
+    tempSum += weatherData[1]["temp"]!;
+    anzahl++;
+  }
+  if (weatherData[2]["temp"] != null) {
+    tempSum += weatherData[2]["temp"]!;
+    anzahl++;
+  }
+  double? avgTemp = tempSum / anzahl;
+
+  // Erstelle leere Liste für Regenmengen
+  double rainSum = 0;
+  anzahl = 0;
+  if (weatherData[0]["rain"] != null) {
+    rainSum += weatherData[0]["rain"]!;
+    anzahl++;
+  }
+  if (weatherData[1]["rain"] != null) {
+    rainSum += weatherData[1]["rain"]!;
+    anzahl++;
+  }
+  if (weatherData[2]["rain"] != null) {
+    rainSum += weatherData[2]["rain"]!;
+    anzahl++;
+  }
+
+  double? avgRain = rainSum / anzahl;
+
   // Erstelle leere Liste für Windgeschwindigkeiten
-  List<double?> winds = [];
-  winds.add(weatherData[0]['wind'] ?? 0);
-  winds.add(weatherData[1]['wind'] ?? 0);
-  winds.add(weatherData[2]['wind'] ?? 0);
-  // Berechne den Durchschnitt der Windgeschwindigkeiten
-  double? avgWind = (winds[0]! + winds[1]! + winds[2]!) / 3;
-  print('Durchschnittliche Windgeschwindigkeit: $avgWind');
+  double windSum = 0;
+  anzahl = 0;
+  if (weatherData[0]["wind"] != null) {
+    windSum += weatherData[0]["wind"]!;
+    anzahl++;
+  }
+  if (weatherData[1]["wind"] != null) {
+    windSum += weatherData[1]["wind"]!;
+    anzahl++;
+  }
+  if (weatherData[2]["wind"] != null) {
+    windSum += weatherData[2]["wind"]!;
+    anzahl++;
+  }
+  double? avgWind = windSum / anzahl;
 
-  print('Wetterdaten: $weatherData');
-  print('Temperaturen: $temps');
-  print('Niederschläge: $rains');
-  print('Windgeschwindigkeiten: $winds');
+  print("Durchschnittstemperatur: $avgTemp");
+
+  print("Durchschnittliche Regenmenge: $avgRain");
+
+  print("Durchschnittliche Windgeschwindigkeit: $avgWind");
+
+  {
+    print("Alle null");
+  }
 }
+
+  //print(temps[0] + temps[1] + temps[2]); // Ausgabe der Summe der Temperaturen
+
+
